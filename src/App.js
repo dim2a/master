6c680c558aea76e2293cf42b0fb65a7bcc390eb3
Буйницкий Дмитрий Викторович (4037557) <buinickii@dev.vtb.ru>
2023-02-22 10:28:20 +0300

    added one more button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   let win;
   let interval;
 
-  const clickHandler = async() => {
+  const clickHandler = async () => {
 
     await setTimeout(() => {
       win = window.open('https://dim2a.github.io/slave/',
@@ -14,8 +14,20 @@ function App() {
         interval = setInterval(() => {
           checkLocation();
         }, 1000);
+    }, 500);    
+  };
+
+  const clickHandler2 = async() => {
+
+    await setTimeout(() => {
+      win = window.open(undefined,
+        '_blank',
+        'location=yes,status=yes,resizable=yes,scrollbars=yes,width=600,height=800');
+        win.location.href = 'https://dim2a.github.io/slave/';
+        interval = setInterval(() => {
+          checkLocation();
+        }, 1000);
     }, 500);
-    console.log('v: 0.1.0');
   };
 
   const checkLocation = () => {
@@ -25,10 +37,12 @@ function App() {
       win.close();
     }
   };
+  console.log('v: 0.1.0');
 
   return (
     <div className="App" >
-      <button style={{width: '100px', height:'50px',}} onClick={clickHandler}>ClickMe!</button>
+      <button style={{width: '100px', height:'50px', backgroundColor:"red"}} onClick={clickHandler}>Button1</button>
+      <button style={{width: '100px', height:'50px', backgroundColor:"green", marginLeft: '5px'}} onClick={clickHandler2}>Button2</button>
     </div>
   );
 }
